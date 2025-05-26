49940b23c9ca9f8fd28e4b6aabbddab523efa05d
unknown <shalbinshaju888@gmail.com>
2025-05-26 11:02:22 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        s1.setRollNo(7);
+        s1.setsName("bhuji");
+        s1.setsAge(28);
+
+        /*Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(org.example.Student.class);
+        cfg.configure();
+        SessionFactory sf = cfg.buildSessionFactory();*/
+
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(org.example.Student.class)
+                .configure()
+                .buildSessionFactory();
+
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(s1);
+        transaction.commit();
+        session.close();
+        sf.close();
+    }
+}
