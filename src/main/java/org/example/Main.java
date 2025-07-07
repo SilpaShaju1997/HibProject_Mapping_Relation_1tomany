7167f91c0b69683cba528a993a324e1a68581ba8
unknown <shalbinshaju888@gmail.com>
2025-07-07 22:34:02 +0200

    learning 07_07_2025

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,26 +10,45 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
 
-        Student s1 = new Student();
-        s1.setRollNo(7);
-        s1.setsName("bhuji");
-        s1.setsAge(28);
+        /*Student s1 = new Student();
+        s1.setRollNo(9);
+        s1.setsName("raju");
+        s1.setsAge(27);*/
 
+        /*Student s2 = null;*/
+        Laptop l1 = new Laptop();
+        l1.setBrand("xxx");
+        l1.setModel("ccc");
+        l1.setRam(32);
+
+        Alien a1 = new Alien();
+        a1.setAid(101);
+        a1.setAname("Thommy");
+        a1.setTech("java");
+        a1.setLaptop(l1);
         /*Configuration cfg = new Configuration();
         cfg.addAnnotatedClass(org.example.Student.class);
         cfg.configure();
         SessionFactory sf = cfg.buildSessionFactory();*/
 
         SessionFactory sf = new Configuration()
-                .addAnnotatedClass(org.example.Student.class)
+                .addAnnotatedClass(org.example.Alien.class)
                 .configure()
                 .buildSessionFactory();
 
         Session session = sf.openSession();
+        /*s1 = session.get(Student.class, 8);*/
         Transaction transaction = session.beginTransaction();
-        session.persist(s1);
+
+        /*session.persist(s1);*/
+        /*session.remove(s1);*/
+        session.persist(a1);
         transaction.commit();
+        Alien a2 = session.get(Alien.class, 101);
+        System.out.println(a2);
+        /*s2 = session.load(Student.class,3);*/
         session.close();
         sf.close();
+        /*System.out.println(s1);*/
     }
 }
